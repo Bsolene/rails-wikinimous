ad460db1a6fa018ce0115fd1c045f755ae215375
solene basaia <bsolene@hotmail.fr>
2017-05-23 17:24:40 +0100

    wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+9.times do
+  article = Article.create(
+    title:    Faker::Superhero.name,
+    content: Faker::ChuckNorris.fact
+  )
+end
